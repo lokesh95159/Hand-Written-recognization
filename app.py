5e0d2866d3d31e16f799a88dfa3f3e7c499e86d2
k.lokesh <113367883+lokesh95159@users.noreply.github.com>
2026-02-18 12:47:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import easyocr
+from PIL import Image
+import numpy as np
+from textblob import TextBlob
+
+# -----------------------------
+# Load EasyOCR
+# -----------------------------
+@st.cache_resource
+def load_ocr():
+    reader = easyocr.Reader(['en'])
+    return reader
+
+reader = load_ocr()
+
+st.title("🧠 Handwritten OCR with Auto Spell Correction")
+
+uploaded_file = st.file_uploader(
+    "Upload Image",
+    type=["png", "jpg", "jpeg"]
+)
+
+if uploaded_file is not None:
+
+    image = Image.open(uploaded_file)
+    image_np = np.array(image)
+
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    # OCR
+    results = reader.readtext(image_np)
+
+    raw_text = ""
+    for (_, text, prob) in results:
+        raw_text += text + " "
+
+    st.subheader("Raw OCR Output:")
+    st.write(raw_text)
+
+    # Spell Correction
+    blob = TextBlob(raw_text)
+    corrected_text = str(blob.correct())
+
+    st.subheader("Corrected Text:")
+    st.success(corrected_text)
